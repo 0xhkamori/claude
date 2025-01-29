bccd468721f47f4e3a5616094c20cf8f4ec34909
hkamori <hk1337@proton.me>
2025-01-29 19:20:19 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -200,86 +200,17 @@ const languageExtensions = {
 };
 
 function detectLanguage(code) {
-    const patterns = {
-        csharp: /(?:using\s+System[;.]|namespace\s+[\w.]+|public\s+class\s+\w+|Console\.|async\s+Task|string\[\]|int\[\]|List<|Dictionary<|IEnumerable<)/,
-        javascript: /(?:const|let|var|function|=>|\bimport\b|\bexport\b|require\()/,
-        python: /(?:def|import|from|class|\bif __name__ == "__main__":|print\()/,
-        java: /(?:public class|void main|System\.out|import java)/,
-        cpp: /(?:#include|std::|cout|cin|void main)/,
-        c: /(?:#include <[^>]+>|printf|scanf|malloc|void main)/,
-        ruby: /(?:def|require|puts|gem|attr_accessor)/,
-        php: /(?:<\?php|\$[a-zA-Z_]|echo|namespace)/,
-        html: /(?:<html|<!DOCTYPE html|<head|<body|<script|<style)/,
-        css: /(?:@media|@keyframes|\{|\}|margin:|padding:|color:)/,
-        typescript: /(?:interface|type [A-Z]|extends|implements)/,
-        rust: /(?:fn main|let mut|impl|pub struct)/,
-        go: /(?:func main|package main|import \(|fmt\.)/,
-        swift: /(?:var|let|func|class|import Foundation)/,
-        kotlin: /(?:fun main|val|var|class|package)/,
-        sql: /(?:SELECT|INSERT INTO|CREATE TABLE|UPDATE|DELETE FROM)/i,
-        scala: /(?:object|def|val|var|trait|extends)/,
-        perl: /(?:use strict|my \$|print|package|sub)/,
-        lua: /(?:function|local|require|end|then)/,
-        r: /(?:library|data\.frame|ggplot|<-|function\()/,
-        matlab: /(?:function|end|plot|figure|matlab)/i,
-        shell: /(?:(?:^|\n)#!\/bin\/|echo|export|source|alias)/,
-        powershell: /(?:Write-Host|Get-|Set-|New-|Remove-)/,
-        fsharp: /(?:let|module|type|member|printfn)/,
-        dart: /(?:void main|Widget|BuildContext|setState)/,
-        groovy: /(?:def|class|extends|implements|package)/,
-        haskell: /(?:module|where|data|type|import)/,
-        julia: /(?:function|end|println|using|module)/,
-        markdown: /(?:^#+ |^\* |\[.+?\]\(.+?\)|^```)/m,
-        xml: /(?:<\?xml|<!DOCTYPE|<(?:[a-z]+:)?[a-z]+>)/i,
-        yaml: /(?:^---|\w+:\s|^-\s)/m,
-        json: /(?:\{|\[)[\s\n]*(?:"[\w\s]+"\s*:)/,
-        dockerfile: /(?:FROM|RUN|COPY|ADD|EXPOSE|ENV)/,
-        elixir: /(?:defmodule|def|do|end|import)/,
-        erlang: /(?:-module|-export|spawn|receive|fun)/,
-        fortran: /(?:PROGRAM|SUBROUTINE|INTEGER|REAL|END)/i,
-        pascal: /(?:program|begin|end|var|procedure)/i,
-        prolog: /(?::-|consult|assert|retract|fail)/,
-        scheme: /(?:\(define|\(lambda|\(let|\(cond)/,
-        vb: /(?:Dim|Private|Public|Class|End)/i,
-        assembly: /(?:section|global|mov|push|pop)/i,
-        sass: /(?:@mixin|@include|@extend|@import|\$[a-z])/,
-        less: /(?:@variable|@import|@media|@\{)/,
-        latex: /(?:\\documentclass|\\begin|\\end|\\usepackage)/,
-        verilog: /(?:module|endmodule|always|initial|wire|reg)/,
-        vhdl: /(?:entity|architecture|process|signal|begin)/i,
-        graphql: /(?:type|query|mutation|interface|input)/,
-        toml: /(?:\[\w+\]|\w+ = )/,
-        ini: /(?:\[\w+\]|\w+=)/
-    };
-
-    if (/^\ufffd\ufffd/.test(code)) {
-        return 'bin';
+    if (!code || typeof code !== 'string') {
+        return 'txt';
     }
 
-    const shebangMatch = code.match(/^#!\s*(.+)/);
-    if (shebangMatch) {
-        const shebang = shebangMatch[1].toLowerCase();
-        if (shebang.includes('node')) return 'javascript';
-        if (shebang.includes('python')) return 'python';
-        if (shebang.includes('ruby')) return 'ruby';
-        if (shebang.includes('perl')) return 'perl';
-        if (shebang.includes('php')) return 'php';
-        return 'shell';
-    }
-
-    const headerComment = code.trim().slice(0, 100).toLowerCase();
-    if (headerComment.includes('<?xml')) return 'xml';
-    if (headerComment.includes('<!doctype html')) return 'html';
-
-    for (const [language, pattern] of Object.entries(patterns)) {
-        if (pattern.test(code)) {
+    const firstLine = code.split('\n')[0].toLowerCase().trim();
+    for (const language of Object.keys(languageExtensions)) {
+        if (firstLine.includes(language)) {
             return language;
         }
     }
-    
-    if (code.trim().startsWith('{') && /^\s*\{[\s\n]*"/.test(code)) return 'json';
-    if (code.trim().startsWith('---') && /:[\s\n]/.test(code)) return 'yaml';
-    
+
     return 'txt';
 }
 
